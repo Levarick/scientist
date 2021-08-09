fc3bdfe35a8c7782f57aa6be61119bdc911113af
Ricardo Leon Valencia <rvalencia765@gmail.com>
2021-08-08 21:22:24 -0500

    route fixed

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
   <div class="header-main">
     <div class="header-logo">
       <h1>
-        <a href="/">
+        <a href="/scientist">
           100tifi.co
         </a>
       </h1>
